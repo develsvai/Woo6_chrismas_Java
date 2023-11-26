836995bbd6917bb49223ed8d5e75fadf243fc3db
hongyongjae <developsvai5096@gmail.com>
2023-11-27 04:39:04 +0900

    feat : View 인터페이스 추가

---
+++ src/main/java/christmas/View/inputView/InputView.java
@@ -0,0 +1,8 @@
+package christmas.View.inputView;
+
+public interface InputView {
+    public String  input_Order_Menu();
+
+    public String input_Expected_Visit_Date();
+
+}
---
+++ src/main/java/christmas/View/outputView/OutputView.java
@@ -0,0 +1,4 @@
+package christmas.View.outputView;
+
+public interface OutputView {
+}
