3b9bcae866c9e4ae0b0ded71154495cdbfb2f1b8
hongyongjae <developsvai5096@gmail.com>
2023-11-27 18:21:26 +0900

    feat : 1차 파싱된 데이터를 모델로 보내 이넘 형태로 변환하여 저장

---
+++ src/main/java/christmas/Model/OrderMenusModel.java
@@ -0,0 +1,30 @@
+package christmas.Model;
+
+import christmas.menu.Menu;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.EnumMap;
+import java.util.List;
+
+public class OrderMenusModel {
+    private final EnumMap<Menu, Integer> OrderMenu;
+
+    public OrderMenusModel(List<SimpleEntry<String, Integer>> Order){
+        OrderMenu = new EnumMap<>(Menu.class);
+
+        Order.forEach(entry -> {
+            Menu menu = Menu.of(entry.getKey());
+            int quantity = entry.getValue();
+            OrderMenu.merge(menu, quantity, Integer::sum);
+        });
+
+    }
+
+    public EnumMap<Menu, Integer> getOrderMenu() {
+        return OrderMenu;
+    }
+
+    /**
+     * 이외 모델 에 대한 검증 로직들
+     */
+
+}
