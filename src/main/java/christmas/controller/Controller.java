77efb134eb06a1965e6da4d370fe9ed5f9cc9b5e
hongyongjae <developsvai5096@gmail.com>
2023-11-27 18:23:14 +0900

    feat : 입력받은 데이터 들 모델 생성 테스트 완료

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,42 +1,44 @@
 package christmas.controller;
 
+import christmas.Service.promotionService.PromotionService;
 import christmas.View.inputView.InputView;
 import christmas.View.outputView.OutputView;
-import christmas.vo.FoodChoice.FoodChoice;
-import christmas.View.inputView.ConsoleInputView;
-import christmas.View.outputView.ConsoleOutputView;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.List;
-import java.util.Map;
+import christmas.dto.InputOrderMenusDto;
+import christmas.dto.InputVisitDateDto;
 
 
 public class Controller {
     private final InputView inputView;
     private final OutputView outputView;
+    private final PromotionService promotionService;
 
-    public Controller(InputView inputView , OutputView outputView) {
+    public Controller(InputView inputView , OutputView outputView, PromotionService promotionService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.promotionService = promotionService;
     }
 
     public void chrisMas_Promotion_Service(){
-        outputView.print_Welcome_Message();
+       outputView.print_Welcome_Message();
+       Input_Expected_Visit_Date();
+       Input_Order_Menu();
+
+       promotionService.printMenu();
 
     }
 
 
     public void Input_Expected_Visit_Date() {
-        inputView.input_Expected_Visit_Date();
-
+        InputVisitDateDto inputVisitDateDto = new InputVisitDateDto(inputView.input_Expected_Visit_Date());
+        promotionService.createVisitDateModel(inputVisitDateDto.takeVisitDate());
     }
 
 
     public void Input_Order_Menu () {
-        List<SimpleEntry<String, Integer>> orderMenu =  inputView.input_Order_Menu();
 
-        for (Map.Entry<String, Integer> entry : orderMenu) {
-            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
-        }
+        InputOrderMenusDto inputOrderMenusDto =  new InputOrderMenusDto(inputView.input_Order_Menu());
+        promotionService.createOrderMenusModel(inputOrderMenusDto.takeInputOrderMenus());
+
     }
 
 
