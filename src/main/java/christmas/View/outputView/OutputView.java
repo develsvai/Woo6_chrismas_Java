34b1a34878c6697430889097a37de5866f171b27
hongyongjae <developsvai5096@gmail.com>
2023-12-04 12:29:03 +0900

    fix & feat : 각 할인 서비스별 출력 추가 및 수정

--- src/main/java/christmas/View/outputView/OutputView.java
+++ src/main/java/christmas/View/outputView/OutputView.java
@@ -1,14 +1,16 @@
 package christmas.View.outputView;
 
+import christmas.Service.AfterDiscountPaymentAmount;
 import christmas.promotionEnum.EventPolicyEnum;
 import christmas.promotionEnum.Menu;
+import java.time.LocalDate;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.EnumMap;
 
 public interface OutputView {
     void print_Welcome_Message();
 
-    void print_PreView_Benefit_Message();
+    void print_PreView_Benefit_Message(LocalDate date);
 
     void print_order_Menu(EnumMap<Menu, Integer> menuIntegerEnumMap);
 
@@ -20,8 +22,8 @@ public interface OutputView {
 
     void print_Total_Benefit_Price( Integer totalAmount);
 
-    void print_After_Discount_Payment_Amount();
+    void print_After_Discount_Payment_Amount(int amount);
 
-    void print_December_Event_Badge();
+    void print_December_Event_Badge( int amount);
 
 }
