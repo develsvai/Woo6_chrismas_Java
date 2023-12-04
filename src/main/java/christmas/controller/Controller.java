e23c0d7314884cef4cd535d590a64b7083852a8a
hongyongjae <developsvai5096@gmail.com>
2023-12-04 12:30:23 +0900

    refactor : 리팩 터링

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -3,11 +3,14 @@ package christmas.controller;
 import christmas.Model.OrderMenusModel;
 import christmas.Model.PromotionDatePolicy;
 import christmas.Model.VisitDateModel;
-import christmas.Service.modelConstructor.Constructor;
+import christmas.Service.AfterDiscountPaymentAmount;
+import christmas.Service.TotalBenefitAmount;
+import christmas.Service.Constructor;
 import christmas.Service.promotionService.DDayDiscountService;
 import christmas.Service.promotionService.GiveAwayEventService;
 import christmas.Service.promotionService.SpecialDiscountService;
 import christmas.Service.promotionService.WeekDayDiscountService;
+import christmas.Service.promotionService.WeekendDiscountService;
 import christmas.View.inputView.InputView;
 import christmas.View.outputView.OutputView;
 import christmas.dto.InputOrderMenusDto;
@@ -31,23 +34,40 @@ public class Controller {
         benefits = new EnumMap<>(EventPolicyEnum.class);
     }
 
-    public void chrisMas_Promotion_Service() {
+    public void chrisMasPromotionPlanner() {
         outputView.print_Welcome_Message();
+
         //모델 생성
         createModel();
+
+        outputView.print_PreView_Benefit_Message(visitDate.takeVisitDate());
+
         //주문 메뉴
         ModelToViewDto modelToViewDto = new ModelToViewDto(orderMenus);
-        outputView.print_order_Menu(modelToViewDto.orderMenuTransferModelToView());
+        outputView.print_order_Menu(modelToViewDto.TransferModelToViewOrderMenu());
+
         //할인전 총 주문 금액
         outputView.print_before_Discount_Total_Price(modelToViewDto.TransferModelToViewTotalAmount());
+
         //증정 메뉴
         giveAwayService();
+
         //할인정책 적용
-        disCountService();
+        applyDiscountService();
+
         //혜택 내역
         outputView.benefits_history(benefits);
 
+        //총 혜택 금액
+        TotalBenefitAmount totalBenefitAmount = new TotalBenefitAmount(benefits);
+        outputView.print_Total_Benefit_Price(totalBenefitAmount.totalBenefitAmount());
 
+        //할인후 예상 결제 금액
+        AfterDiscountPaymentAmount afterDiscountPaymentAmount = new AfterDiscountPaymentAmount(orderMenus, benefits);
+        outputView.print_After_Discount_Payment_Amount(afterDiscountPaymentAmount.getAfterDiscountPaymentAmount());
+
+        //12월 이벤트 배지
+        outputView.print_December_Event_Badge(totalBenefitAmount.totalBenefitAmount());
 
     }
 
@@ -70,7 +90,7 @@ public class Controller {
     }
 
 
-    private void disCountService(){
+    private void applyDiscountService(){
         //디데이 할인
         DDayDiscountService dDayDiscountService = new DDayDiscountService();
         SimpleEntry<EventPolicyEnum, Integer> dDayResult = dDayDiscountService.discountService(orderMenus, visitDate, policy);
@@ -81,14 +101,16 @@ public class Controller {
         SimpleEntry<EventPolicyEnum, Integer> weekDayResult = weekDayDiscountService.discountService(orderMenus,visitDate,policy);
         benefits.put(weekDayResult.getKey(),weekDayResult.getValue());
 
+        //주말 할인
+        WeekendDiscountService weekendDiscountService = new WeekendDiscountService();
+        SimpleEntry<EventPolicyEnum, Integer> weekendResult = weekendDiscountService.discountService(orderMenus, visitDate, policy);
+        benefits.put(weekendResult.getKey(), weekendResult.getValue());
+
         //특별 할인
         SpecialDiscountService specialDiscountService = new SpecialDiscountService();
         SimpleEntry<EventPolicyEnum, Integer> specialResult = specialDiscountService.discountService(orderMenus, visitDate,policy);
         benefits.put(specialResult.getKey(), specialResult.getValue());
     }
-
-
-
 
 
     private InputVisitDateDto Input_Expected_Visit_Date() {
